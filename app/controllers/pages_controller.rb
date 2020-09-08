4b1baed753daedb199ee48df8098e619a707f0ba
Stéphane ZE-OGIER <contact@ze-ogier.com>
2020-09-09 01:02:09 +0200

    first commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    general = Chatroom.find_by(name: "general")
+    redirect_to chatroom_path(general) if general
   end
 end
